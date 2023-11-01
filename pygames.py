78b184c369be642e114ad570cb667409d780fd38
Jackson Lorran <computadordb@gmail.com>
2023-11-01 08:47:42 -0300

    Adicionando movimento a objetos

--- pygames.py
+++ pygames.py
@@ -8,13 +8,20 @@ pygame.init()
 
 largura = 640
 altura = 480
+x=largura/2
+y=0
 #criando a tela de exibição do jogo parametrizado por largura e altura
 tela = pygame.display.set_mode((largura,altura))
 #Renomeando o nome da janela que exibirá o jogo
 pygame.display.set_caption('Jogo')
 
+relogio = pygame.time.Clock()
+
+
 #Loop principal do jogo para que ele possa continuar
 while True:
+    relogio.tick(120)
+    tela.fill((0,0,0))
     #Para cada evento que acontecer no jogo
     for event in pygame.event.get():
         #Se o evento for sair do jogo ele irá fechar
@@ -23,14 +30,19 @@ while True:
             exit()
             #Criando um retângulo no jogo
                     #Tela / Cor do objeto / posição do objeto + especificações
-    pygame.draw.rect(tela,(255,0,0),(200,300,40,50))
+    pygame.draw.rect(tela,(255,0,0),(x,y,40,50))
+    if y>= altura:
+        y=0
+    y=y+5
             #Criando um circulo no jogo
-                    # Tela / Cor do objeto / posição do objeto + raio
+    """
+               # Tela / Cor do objeto / posição do objeto + raio
     pygame.draw.circle(tela,(0,0,255),(300,260),40)
             #Desenhando uma linha na tela do jogo
                     #Tela / cor da linha / Começo da linha / Final da linha / tamanho da linha
     pygame.draw.line(tela, (255,255,0),(390,0),(390,600),6)
     #Atualizando o display após o término de todos os eventos
+    """
     pygame.display.update()
 
 
