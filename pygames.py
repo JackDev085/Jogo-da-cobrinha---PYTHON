8e38775c49e1876740b3f4d28adf7478c878cbe5
Jackson Lorran <computadordb@gmail.com>
2023-11-10 10:52:01 -0300

    Adicionando novas funcionalidades

--- pygames.py
+++ pygames.py
@@ -7,7 +7,7 @@ from random import randint
 #Iniciando o construtor para construção do jogo
 pygame.init()
 #Adicionando o valor do volume para a música de fundo
-pygame.mixer.music.set_volume(0.6)
+pygame.mixer.music.set_volume(0.1)
 #Atribuindo a variável musica_fundo uma música de fundo
 musica_fundo = pygame.mixer.music.load('musica.mp3')
 
@@ -15,16 +15,21 @@ pygame.mixer.music.play(-1)
 #Adicionando um barulho para som de colisão
 barulho_colisao = pygame.mixer.Sound('sm64_coin.wav')
 
-#barulho_colisao.set_volume(0.6) barulo do som de colisão
+barulho_colisao.set_volume(0.05)
 largura = 640
 altura = 480
-x=int(largura/2)
-y=int((altura/2))
+x_cobra=int(largura/2)
+y_cobra=int((altura/2))
 
-x_azul = randint(40,600)
-y_azul = randint(50,430)
+velocidade = 10
+x_controle = velocidade
+y_controle = 0
 
+x_maca = randint(40,600)
+y_maca = randint(50,430)
+x_cobra
 pontos = 0
+
 fonte = pygame.font.SysFont('times new roman', 40,True,True)
 #criando a tela de exibição do jogo parametrizado por largura e altura
 tela = pygame.display.set_mode((largura,altura))
@@ -33,61 +38,85 @@ pygame.display.set_caption('Jogo')
 #Adiciondo um "Relógio" que vai ser usado como
 #tempo do jogo
 relogio = pygame.time.Clock()
+lista_cobra = []
+comprimento_inicial = 5
+def aumenta_cobra(lista_cobra):
+    for xey in lista_cobra:
 
-
+        pygame.draw.rect(tela, (0,255,0), (xey[0],xey[1],20,20))
 #Loop principal do jogo para que ele possa continuar
 while True:
     #Adiciona o tempo de velocidade do jogo
     relogio.tick(30)
     #Preenche toda a tela com a cor especificada
-    tela.fill((0,0,0))
+    tela.fill((255,255,255))
 
     mensagem = f'Pontos: {pontos}'
-    texto_formatado = fonte.render(mensagem, True, (255,255,255))
+    texto_formatado = fonte.render(mensagem, True, (0,0,0))
     #Para cada evento que acontecer no jogo
     for event in pygame.event.get():
         #Se o evento for sair do jogo ele irá fechar
         if event.type == QUIT:
             pygame.quit()
             exit()
-            """
             #Movimentações de objetos
         if event.type == KEYDOWN:
             if event.key == K_a:
-                x = x - 20
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle =  0
             if event.key == K_d:
-                x = x + 20
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
             if event.key == K_w:
-                y = y - 20
+                if y_controle == velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = -velocidade
             if event.key == K_s:
-                y = y + 20
-                """
-    #Movimentando o objeto de forma constante
-    #utilizando as teclas
-    if pygame.key.get_pressed()[K_a]:
-        x = x - 20
-    if pygame.key.get_pressed()[K_d]:
-        x = x + 20
-    if pygame.key.get_pressed()[K_s]:
-        y = y + 20
-    if pygame.key.get_pressed()[K_w]:
-        y = y - 20
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = 0
+                    y_controle = velocidade
+
+    x_cobra = x_cobra+x_controle
+    y_cobra = y_cobra+y_controle
+
     # Criando um retângulo no jogo
     # Tela / Cor do objeto / posição do objeto + especificações
-    ret_azul = pygame.draw.rect(tela,(255,0,0),(x,y,40,50))
+    cobra = pygame.draw.rect(tela,(0,255,0),(x_cobra,y_cobra,20,20))
     #Criando um novo retângulo
-    ret_vermelho = pygame.draw.rect(tela, (0, 0, 255), (x_azul, y_azul, 40, 50))
+    comida = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
 
     #Adicionando um evento de colisão
-    if ret_vermelho.colliderect(ret_azul):
-        x_azul = randint(40,600)
-        y_azul = randint(50,430)
+    if cobra.colliderect(comida):
+        x_maca = randint(40,600)
+        y_maca = randint(50,430)
         #Incrementando os pontos quando houver colisão
         pontos = pontos + 1
         #Toda vez que ouver uma colisão será tocado o  som que estiver atribuido a barulho_colisao
         barulho_colisao.play()
+        comprimento_inicial = comprimento_inicial + 1
     #Criando um circulo no jogo
+
+    lista_cabeca=[]
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+
+    lista_cobra.append(lista_cabeca)
+
+    if len(lista_cobra) > comprimento_inicial:
+        del lista_cobra[0]
+    aumenta_cobra(lista_cobra)
     """
+
                # Tela / Cor do objeto / posição do objeto + raio
     pygame.draw.circle(tela,(0,0,255),(300,260),40)
             #Desenhando uma linha na tela do jogo
